78588d9847d63172f4937bc5a2c695e63990185f
OZKAN <ozkanismail@mail.ru>
2021-11-29 18:43:44 +0300

    ff

--- src/main/java/com/iozkan/userservice/UserserviceApplication.java
+++ src/main/java/com/iozkan/userservice/UserserviceApplication.java
@@ -26,27 +26,27 @@ public class UserserviceApplication {
 		return new BCryptPasswordEncoder();
 	}
 
-	/*@Bean
+	@Bean
 	CommandLineRunner run(UserService userService){
 		return args -> {
-			userService.saveRole(new Role(null,"ROLE_USER"));
-			userService.saveRole(new Role(null,"ROLE_MANAGER"));
+			//userService.saveRole(new Role(null,"ROLE_USER"));
+			//userService.saveRole(new Role(null,"ROLE_MANAGER"));
 			userService.saveRole(new Role(null,"ROLE_ADMIN"));
-			userService.saveRole(new Role(null,"ROLE_SUPER_ADMIN"));
+			//userService.saveRole(new Role(null,"ROLE_SUPER_ADMIN"));
 
 			userService.saveUser(new User(null,"İsmail Özkan","ismail16","1234",new ArrayList<>()));
-			userService.saveUser(new User(null,"Akif Özkan","akif16","1234",new ArrayList<>()));
-			userService.saveUser(new User(null,"Nefise Özkan","nefise16","1234",new ArrayList<>()));
-			userService.saveUser(new User(null,"Muammer Özkan","muammer16","1234",new ArrayList<>()));
+			//userService.saveUser(new User(null,"Akif Özkan","akif16","1234",new ArrayList<>()));
+			//userService.saveUser(new User(null,"Nefise Özkan","nefise16","1234",new ArrayList<>()));
+			//userService.saveUser(new User(null,"Muammer Özkan","muammer16","1234",new ArrayList<>()));
 
-			userService.addRoleToUser("ismail16","ROLE_USER");
-			userService.addRoleToUser("ismail16","ROLE_MANAGER");
+			//userService.addRoleToUser("ismail16","ROLE_USER");
+			//userService.addRoleToUser("ismail16","ROLE_MANAGER");
 			userService.addRoleToUser("ismail16","ROLE_ADMIN");
-			userService.addRoleToUser("ismail16","ROLE_SUPER_ADMIN");
+			//userService.addRoleToUser("ismail16","ROLE_SUPER_ADMIN");
 
-			userService.addRoleToUser("akif16","ROLE_USER");
-			userService.addRoleToUser("nefise16","ROLE_USER");
-			userService.addRoleToUser("muammer16","ROLE_USER");
+			//userService.addRoleToUser("akif16","ROLE_USER");
+			//userService.addRoleToUser("nefise16","ROLE_USER");
+			//userService.addRoleToUser("muammer16","ROLE_USER");
 		};
-	}*/
+	}
 }
